7731c61c5f111ac42a34a68dcac98e0098f8dce8
shakhawat hossain <shakhwt@gmail.com>
2021-01-05 18:34:29 +0600

    post request succeed from api

--- LoginSession/ViewController.swift
+++ LoginSession/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var phoneNoText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    let parameters: Parameters = ["mobile_number": "+9641920961634", "password": "Password@2"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        apiHandler()
     }
+    @IBAction func didTapedLogin(_ sender: Any) {
+        
+    }
+    
+    func apiHandler () {
+        let urlString = "https://staging-apigw-personal.fast-pay.cash/api/v1/auth/signin"
 
+        AF.request(urlString, method: .post, parameters: parameters,encoding: JSONEncoding.default, headers: nil).responseJSON {
+        response in
+          switch response.result {
+                        case .success:
+                            print(response)
+
+                            break
+                        case .failure(let error):
+
+                            print(error)
+                        }
+        }
+    }
+    
+    
+    @objc func tapOnView(_ tapGestureRecognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+        view.removeGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let gestureView = UITapGestureRecognizer(target: self, action: #selector(tapOnView(_:)))
+        view.addGestureRecognizer(gestureView)
+    }
 
 }
 
