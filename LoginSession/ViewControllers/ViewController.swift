263d562a9c20eadd282ebf85b0a51b0be5b780ab
shakhawat hossain <shakhwt@gmail.com>
2021-01-10 14:17:31 +0600

    APi Called

--- LoginSession/ViewControllers/ViewController.swift
+++ LoginSession/ViewControllers/ViewController.swift
@@ -10,47 +10,55 @@ import Alamofire
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var phoneNoText: UITextField!
-    @IBOutlet weak var passwordText: UITextField!
-    @IBOutlet weak var logInButton: UIButton!
+@IBOutlet weak var phoneNoText: UITextField!
+@IBOutlet weak var passwordText: UITextField!
+@IBOutlet weak var logInButton: UIButton!
+
+    var parameters: Parameters?
+
+override func viewDidLoad() {
+    super.viewDidLoad()
     
-    let parameters: Parameters = ["mobile_number": "+9641920961634", "password": "Password@2"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        apiHandler()
     }
-    @IBAction func didTapedLogin(_ sender: Any) {
-        
+@IBAction func didTapedLogin(_ sender: Any) {
+    guard let phoneNum = self.phoneNoText.text, let pass = self.passwordText.text else {
+        print("your info is nil.")
+        return
     }
-    
+    parameters = ["mobile_number": "\(phoneNum)", "password": "\(pass)"]
+    apiHandler()
+}
     func apiHandler () {
-        let urlString = "https://staging-apigw-personal.fast-pay.cash/api/v1/auth/signin"
+    let urlString = "https://staging-apigw-personal.fast-pay.cash/api/v1/auth/signin"
 
-        AF.request(urlString, method: .post, parameters: parameters,encoding: JSONEncoding.default, headers: nil).responseJSON {
-        response in
-          switch response.result {
-                        case .success:
-                            print(response)
+    AF.request(urlString, method: .post, parameters: parameters,encoding: JSONEncoding.default, headers: nil).responseJSON { response in
+    switch response.result {
+                case .success:
+                    print(response)
+                    do {
+                    let data = try JSONDecoder().decode(singin_model.self, from: response.data!)
+                        print(data.data?.token!)
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "home")
+                        self.navigationController?.pushViewController(vc!, animated: true)
+                        
+                    }catch {
+                        print("not working")
+                    }
 
-                            break
-                        case .failure(let error):
+                    break
+                case .failure(let error):
 
-                            print(error)
-                        }
-        }
+                    print(error)
+                }
     }
-    
-    
-    @objc func tapOnView(_ tapGestureRecognizer: UITapGestureRecognizer) {
-        view.endEditing(true)
-        view.removeGestureRecognizer(tapGestureRecognizer)
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        let gestureView = UITapGestureRecognizer(target: self, action: #selector(tapOnView(_:)))
-        view.addGestureRecognizer(gestureView)
     }
 
 }
 
+struct singin_model: Codable {
+    var data : token_model?
+}
+
+struct token_model: Codable {
+    var token : String?
+}
